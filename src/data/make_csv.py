97a78ce44e8755b0ea1598214c0c6f102b8aa0db
joshcarty <carty.josh@gmail.com>
2018-05-22 07:48:05 +0100

    Limit CSV to only available files.

--- src/data/make_csv.py
+++ src/data/make_csv.py
@@ -1,5 +1,6 @@
 import argparse
 import json
+import os
 
 import pandas as pd
 
@@ -10,14 +11,25 @@ def load_json(path):
     return labels
 
 
-def create_dataframe(labels):
+def load_filenames(path):
+    files = os.listdir(path)
+    files = [file.split('.')[0]
+             for file in files
+             if file.endswith('.jpg')]
+    return files
+
+
+def create_dataframe(labels, dataset):
     df = pd.DataFrame(labels)
+    files = load_filenames(dataset)
+    df = df[df['imageId'].isin(files)]
     df['labelId'] = df['labelId'].apply(" ".join)
     return df
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset', required=True)
     parser.add_argument('--inpath', required=True)
     parser.add_argument('--outpath', required=True)
     return parser.parse_args()
@@ -26,7 +38,7 @@ def parse_args():
 def main():
     args = parse_args()
     labels = load_json(args.inpath)
-    df = create_dataframe(labels)
+    df = create_dataframe(labels, args.dataset)
     df.to_csv(args.outpath, index=False)
 
 
